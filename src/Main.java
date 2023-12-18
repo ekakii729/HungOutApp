c5865e2fec7ca00af500c3c81527a604777021bf
Abhay Manoj <sanicthecheese@gmail.com>
2023-12-18 14:29:33 -0500

    Cleaned up shit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        System.out.println(x);
-        System.out.println("My name is Abhay");
-        System.out.println("OK NIGG");
+
     }
 }
