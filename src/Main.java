6ae80821d8048f0c4735a394c22b23b7b9eb9586
Abhay Manoj <sanicthecheese@gmail.com>
2023-12-19 16:53:05 -0500

    added user class + class comments

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/*
+ * Creator(s): Abhay Manoj
+ * Date of Creation: December 19th, 2023
+ * Purpose: ADD LATER
+ */
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/User.java
@@ -0,0 +1,9 @@
+/*
+ * Creator(s): Abhay Manoj
+ * Date Of Creation: December 19th, 2023
+ * Purpose: This class represents a user of the program
+ */
+
+public class User {
+
+}
