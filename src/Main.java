030a155084eb504f09b6cc40a55cb1d3e4f4db9b
Abhay Manoj <sanicthecheese@gmail.com>
2023-12-19 20:19:48 -0500

    Added some testing code

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,41 @@
  * Purpose: ADD LATER
  */
 
-public class Main {
-    public static void main(String[] args) {
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import net.miginfocom.swing.MigLayout;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public void run() {
+        setSize(400,400);
+
+        JPanel panel = new JPanel(new MigLayout());
+
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"), "wrap");
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"), "wrap");
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"));
+        panel.add(new JButton("Hello!"));
+
+        add(panel);
+        setVisible(true);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        FlatDarculaLaf.setup();
+
+        Main main = new Main();
+        main.run();
     }
 }
