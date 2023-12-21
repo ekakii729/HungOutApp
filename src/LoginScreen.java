63a934489af8be413f5cbde8b330f6bd160fa7a3
Abhay Manoj <sanicthecheese@gmail.com>
2023-12-20 21:58:26 -0500

    Made imports alphabetical;

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -4,17 +4,18 @@
  * Purpose: This is the log in screen for the program
  */
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JTextField;
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class LoginScreen extends JFrame implements ActionListener {
 
--- src/SignupScreen.java
+++ src/SignupScreen.java
@@ -4,17 +4,17 @@
  * Purpose: This is the sign-up screen for the program
  */
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class SignupScreen extends JFrame implements ActionListener {
 
